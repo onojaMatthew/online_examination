ee6c016eeb5cda16e29732fa0c66ff2192b8a958
onojaMatthew <onojamatthew59@gmail.com>
2021-08-31 05:27:29 +0100

    added new updates

--- client/src/components/Admin/Dashboard/Question/RandomQuestions.js
+++ client/src/components/Admin/Dashboard/Question/RandomQuestions.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { ModalHeader, Modal, ModalBody, ModalFooter, Input } from "reactstrap";
 import { Button } from "antd";
 
 const RandomQuestions = ({ questions, randomModal, toggleRandomModal }) => {
+  const [ random, setRandom ] = useState("");
+  const { docs } = questions
   console.log(questions, " the question inside modal")
   return (
     <div>
       <Modal isOpen={randomModal} toggle={toggleRandomModal} id="new-question-modal">
         <ModalHeader toggle={toggleRandomModal} id="modal-header">Randomize Questions</ModalHeader>
         <ModalBody>
-          
+          {docs && docs.length > 0 ? docs.map((q, i) => (
+            <div className="random-list">
+              <Input type="checkbox" value={q._id} /> {q?.question}
+            </div>
+          )) : <h3 className="text-center mt-5">No Questions</h3>}
+          <div className="random">
+              <Input value={random} placeholder="Randomization number" />
+            </div>
+
         </ModalBody>
         <ModalFooter>
           {/* {create_loading ? <Button className="submit-button" loading>Processing...</Button> : 
