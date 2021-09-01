6780a2682967bd455cc6952d11ae3853b6f40e0f
onojaMatthew <onojamatthew59@gmail.com>
2021-09-01 13:55:38 +0100

    updated

--- src/controllers/admin/router.js
+++ src/controllers/admin/router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAdmin, dashboardData, fetchAdmin, signIn } from "./admin";
+import { createAdmin, dashboardData, fetchAdmin, logout, signIn } from "./admin";
 
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/admin/post", createAdmin);
 router.post("/admin/login", signIn);
 router.get("/admin/list", fetchAdmin);
+router.get("/admin/logout", logout)
 router.get("/admin/dashboard", dashboardData);
 
 export default router;
